785909d0a8156df099790b14acf429fd93effd7a
Eva Doncieux <eva.doncieux@gmail.com>
2024-09-26 23:26:43 +0200

    [fix] removing doctrine broadcast and mercure cause i don't use them for the moment
    Took 2 minutes

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\UX\Translator\UxTranslatorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
