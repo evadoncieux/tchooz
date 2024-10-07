7dfa7cf47c715abceb9b6efbbf33d10b4fcde94a
Eva Doncieux <eva.doncieux@gmail.com>
2024-10-07 14:07:59 +0200

    [data & features] improving data fetching and adding Recommandations controller and service (wip)
    Took 2 hours 46 minutes
    
    Took 2 minutes

--- src/Service/Data/WeatherDataService.php
+++ src/Service/Data/WeatherDataService.php
@@ -3,18 +3,25 @@
 namespace App\Service\Data;
 
 use App\Entity\DailyWeather;
+use App\Service\Api\LocationApiService;
 use App\Service\Api\WeatherApiService;
 use DateTime;
+use DateTimeZone;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 readonly class WeatherDataService
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
         private WeatherApiService      $weatherApiService,
+        private LocationApiService $locationApiService,
         private LoggerInterface        $logger,
     )
     {
@@ -32,10 +39,21 @@ readonly class WeatherDataService
         }
 
         $weatherData = new DailyWeather();
-        $weatherData->setHumidity($weatherApiData['current']['humidity'])
-            ->setTemperature($weatherApiData['current']['temp_c'])
-            ->setWeatherType()
-            ->setTimestamp(new DateTime());
+        $weatherData
+            ->setName($weatherApiData['weather'][0]['main'])
+            ->setTemperature($weatherApiData['main']['temp'])
+            ->setDescription($weatherApiData['weather'][0]['description'])
+            ->setWindSpeed($weatherApiData['wind']['speed'])
+            ->setCity($weatherApiData['name'])
+            ->setTempFeels($weatherApiData['main']['feels_like'])
+            ->setTempMin($weatherApiData['main']['temp_min'])
+            ->setTempMax($weatherApiData['main']['temp_max'])
+            ->setHumidity($weatherApiData['main']['humidity']);
+        if (isset($weatherApiData['rain'])) {
+            $weatherData->setRain($weatherApiData['rain']['1h']);
+        }
+        $weatherData
+            ->setTimestamp(new DateTime('now', new DateTimeZone('Europe/Paris')));
 
         $this->entityManager->persist($weatherData);
         $this->entityManager->flush();
@@ -43,9 +61,18 @@ readonly class WeatherDataService
         return $weatherData;
     }
 
-    public function getDailyWeather(): ?DailyWeather
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws \JsonException
+     */
+    public function getDailyWeather(string $location): ?DailyWeather
     {
+        $coordinates = $this->locationApiService->getCoordinates($location);
+        $cityName = $coordinates[0]['name'];
         return $this->entityManager->getRepository(DailyWeather::class)
-            ->findOneBy([], ['timestamp' => 'DESC']);
+            ->findOneBy(['city' => $cityName], ['timestamp' => 'DESC']);
     }
 }
