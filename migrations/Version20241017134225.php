13ad5adfed6f5100adac9da0dceb5a20750decef
Eva Doncieux <eva.doncieux@gmail.com>
2024-10-17 15:43:04 +0200

    [fix] fixing user registration
    Took 4 minutes

--- migrations/Version20241017134225.php
+++ migrations/Version20241017134225.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20241017043535 extends AbstractMigration
+final class Version20241017134225 extends AbstractMigration
 {
     public function getDescription(): string
     {
